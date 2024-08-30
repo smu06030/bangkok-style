39a8d995062f2e318734b601fbe11b4c2cee0b63
Bsjaehee <bsjaehee94@gmail.com>
2024-08-30 20:21:31 +0900

    add: 댓글 추가, 좋아요 상태 표시 기능

--- src/pages/public/Detail.jsx
+++ src/pages/public/Detail.jsx
@@ -7,29 +7,52 @@ const Detail = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const postId = queryParams.get("id");
-  console.log(postId);
   const { posts } = useContext(EntireContext);
-  console.log("Posts:", posts);
   const post = posts.posts.find((post) => post.id === Number(postId));
+  const [inputVal, setInputVal] = useState("");
+  const [comments, setComments] = useState([]);
 
   if (!post) return <div>해당 post를 찾을 수 없습니다.</div>;
 
+  const handleAddComment = () => {
+    if (inputVal.trim()) {
+      setComments((prev) => [...prev, inputVal]);
+      setInputVal("");
+    }
+  };
   return (
     <OuterDiv>
       <PostDiv>
-        <span>id: {post.id}</span>
+        <span>id: {post.user_id}</span>
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5QPTO0dfQhrhSVMfjUTjj-7uh1zyqNnjYCg&s"
           alt=""
         />
         {/* <img src={post.img_url} alt="" /> */}
-        <span style={{ marginTop: "5px" }}>❤️</span>
+        <span style={{ marginTop: "5px" }}>
+          {post.like ? (
+            <Heart src="https://velog.velcdn.com/images/bsjaehee94/post/589d104f-1104-43db-980a-548d879ef168/image.png" />
+          ) : (
+            <Heart src="https://velog.velcdn.com/images/bsjaehee94/post/09517912-428c-4edb-871e-8b474c141ad9/image.png" />
+          )}
+        </span>
         <span>+ 99</span>
         <span>{post.hash_tag}</span>
         <span>{post.content}</span>
         <div style={{ display: "flex", gap: "5px" }}>
-          <CommentInput placeholder="댓글을 입력해주세요" />
-          <button>추가</button>
+          <CommentInput
+            placeholder="댓글을 입력해주세요"
+            value={inputVal}
+            onChange={(event) => {
+              setInputVal(event.target.value);
+            }}
+          />
+          <button onClick={handleAddComment}>추가</button>
+        </div>
+        <div>
+          {comments.map((comment, index) => (
+            <p key={index}>{comment}</p>
+          ))}
         </div>
       </PostDiv>
     </OuterDiv>
@@ -70,4 +93,9 @@ const CommentInput = styled.input`
   ::placeholder {
     font-size: 14px;
   }
+`;
+
+const Heart = styled.img`
+  width: 20px;
+  height: 20px;
 `;
