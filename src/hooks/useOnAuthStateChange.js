88e560bcc64d037255b1d34e7111044d768faa14
WonbinLee <wonbin145@gmail.com>
2024-09-02 16:25:15 +0900

    pull

--- src/hooks/useOnAuthStateChange.js
+++ src/hooks/useOnAuthStateChange.js
@@ -5,8 +5,10 @@ import supabase from "../supabaseClient";
 // 유저정보를 context에 저장하는 함수
 const useOnAuthStateChange = () => {
   const { signIn } = useContext(EntireContext);
+
   useEffect(() => {
-    const { data: authListener } = supabase.auth.onAuthStateChange((_, session) => {
+    const { data: authListener } = supabase.auth.onAuthStateChange((action, session) => {
+      console.log("action", action);
       session && signIn(session.user);
     });
 
