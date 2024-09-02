c28da297b04a3d1b29cd44e8e5c44e824b1f2335
hadooni <hayoung7259@gmail.com>
2024-09-02 19:07:15 +0900

    feat: 비밀번호 변경 기능 추가

--- src/components/MyPage/ChangePassword.jsx
+++ src/components/MyPage/ChangePassword.jsx
@@ -1,44 +1,100 @@
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import EntireContext from "../../Context/EntireContext";
 import styled from "styled-components";
+import supabase from "../../supabaseClient";
+import { PASSWORD_REGEX } from "../../constant/regularExpression";
+import { toast } from "sonner";
 
 const IdBox = styled.div`
   display: flex;
 `;
 
+// const PasswordContainer = styled.div`
+
+// `
+
 const ChangePassword = () => {
-  const [password, SetPassword] = useState("");
+  // 비밀번호, 비밀번호 확인
   const [newPassword, setNewPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
   const { userInfo } = useContext(EntireContext);
-  console.log("userInfo :>> ", userInfo);
-  // const { data, error } = await supabase.auth.updateUser({
-  //     password: "new-password",
-  //   })
+  const userProvider = userInfo.app_metadata.provider;
+
+  // 변경 비밀번호
+  const onChangePassword = (e) => {
+    setNewPassword(e.target.value);
+  };
+
+  // 비밀번호 확인
+  const onChangeCheckPassword = (e) => {
+    setCheckPassword(e.target.value);
+  };
+
+  console.log("newPassword :>>", newPassword);
+  console.log("checkPassword :>> ", checkPassword);
+  console.log(newPassword === checkPassword);
+
+  // 비밀번호 변경 실행
+  const handleChangePassword = async () => {
+    if (newPassword === checkPassword) {
+      await supabase.auth.updateUser({
+        password: newPassword
+      });
+      toast.success("비밀번호가 변경되었습니다.");
+    } else {
+      toast.error("비밀번호가 일치하지 않습니다.");
+    }
+  };
+
+  //   const handleCancel = () => {
+  //     return;
+  //   };
+
+  const handleToggle = () => {};
 
   return (
     <>
-      <button>회원정보 수정</button>
-      <div>
-        <p>회원정보 수정</p>
-        <IdBox>
-          <p>▸ 아이디</p>
-          <p>{userInfo.email}</p>
-        </IdBox>
-        <div>
-          <label htmlFor="password">▸ 현재 비밀번호</label>
-          <input type="password" id="password" />
-        </div>
-        <div>
-          <label htmlFor="new-password">▸ 변경 비밀번호</label> <input type="password" id="new-password" />
-        </div>
-        <p>숫자, 영어, 특수문자를 포함한 8자 이상, 15자 이하로 입력해주세요.</p>
-        <div>
-          <label htmlFor="check-password">▸ 비밀번호 확인</label>
-          <input type="password" id="check-password" />
-        </div>
-        <button>취소</button>
-        <button>수정</button>
-      </div>
+      {userProvider === "email" ? (
+        <>
+          <button onClick={handleToggle}>회원정보 수정</button>
+          <div>
+            <p>회원정보 수정</p>
+            <IdBox>
+              <p>▸ 아이디</p>
+              <p>{userInfo.email}</p>
+            </IdBox>
+            <div>
+              <label htmlFor="new-password">▸ 변경 비밀번호</label>
+              <input
+                type="password"
+                id="new-password"
+                value={newPassword}
+                onChange={onChangePassword}
+                placeholder="비밀번호"
+              />
+            </div>
+            <p>
+              {(newPassword.length > 0 && PASSWORD_REGEX.test(newPassword)) ||
+                "비밀번호는 숫자, 영어, 특수문자를 포함한 8자 이상 15자 이하입니다."}
+            </p>
+            <div>
+              <label htmlFor="check-password">▸ 비밀번호 확인</label>
+              <input
+                type="password"
+                id="check-password"
+                value={checkPassword}
+                onChange={onChangeCheckPassword}
+                placeholder="비밀번호 확인"
+              />
+            </div>
+            <p>{newPassword === checkPassword ? "비밀번호가 일치합니다." : "비밀번호가 일치하지 않습니다."}</p>
+            {/* <button>취소</button> */}
+            <button onClick={handleChangePassword}>수정</button>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
