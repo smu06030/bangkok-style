5d3294df11f39c13e3b7304d05c1c8b8e6c53562
WonbinLee <wonbin145@gmail.com>
2024-09-04 10:57:49 +0900

    move:CommentSection 파일 위치 components/Layout으로 이동

--- src/services/getAllDataService.js
+++ src/services/getAllDataService.js
@@ -1,19 +1,14 @@
 import supabase from "../supabaseClient";
 
 const getAllData = async () => {
-  try{
-    // const selectFields = userId ? "*, likes(post_id)" : "*";
-    
+  try {
     // 전체 게시글 데이터 가져오기
-    const response = await supabase
-      .from("posts")
-      .select("*, likes(post_id)")
-      .order("created_at", { ascending: false })
-      
-    return response
+    const response = await supabase.from("posts").select("*, likes(post_id)").order("created_at", { ascending: false });
+
+    return response;
   } catch (error) {
     console.error("게시글 데이터를 가져오지 못했습니다.", error);
   }
-}
+};
 
 export default getAllData;
